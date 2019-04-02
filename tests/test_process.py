2128caa462006e413e549456cb8c09561d07fd3e
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-04-02 14:58:19 +0200

    Better steps chain

--- tests/test_process.py
+++ tests/test_process.py
@@ -4,6 +4,7 @@ import cv2
 import pytest
 import shutil
 import json
+import re
 
 
 def load_image(image_name):
@@ -43,23 +44,23 @@ def check_image(root_folder, image, name):
     assert score > 0.999, '{} ({}) != {} ({})'.format(expected, result, expected_name, image)
 
 
-@pytest.mark.parametrize('type_,limit', [
+@pytest.mark.parametrize('type_,limit,size', [
     ('lines', {
         'name': 'VL0',
         'type': 'line detection',
         'value': 1821,
         'vertical': True,
         'margin': 0
-    }),
+    }, '543 x 792 pts'),
     ('contour', {
         'name': 'VC0',
         'type': 'contour detection',
         'value': 1588,
         'vertical': True,
         'margin': 0
-    })
+    }, '550 x 792 pts')
 ])
-def test_assisted_split_full(type_, limit):
+def test_assisted_split_full(type_, limit, size):
     os.environ['PROCESS'] = 'TRUE'
     os.environ['EXPERIMENTAL'] = 'FALSE'
     root_folder = '/results/assisted-split-full-{}/'.format(type_)
@@ -74,12 +75,18 @@ def test_assisted_split_full(type_, limit):
         'args': {
             'assisted_split': True,
             'level': True,
+            'append_credit_card': False
         },
-        'images': ['image-1.png']
+        'full_name': 'Test title 1',
+        'destination': os.path.join(root_folder, 'final.pdf'),
+    }
+    step = {
+        'sources': ['image-1.png'],
     }
     config_file_name = os.path.join(root_folder, 'config.yaml')
-    images = process.transform(config, config_file_name, root_folder)
-    config['transformed_images'] = images
+    step = process.transform(config, step, config_file_name, root_folder)
+    assert step['name'] == 'split'
+    images = step['sources']
     assert os.path.basename(images[0]) == config['assisted_split'][0]['image']
     assert len(images) == 1
     check_image(root_folder, images[0], 'assisted-split-{}-1'.format(type_))
@@ -90,11 +97,24 @@ def test_assisted_split_full(type_, limit):
     limits = [item for item in limits if item['name'] == limit['name']]
     assert limits == [limit]
     config['assisted_split'][0]['limits'] = limits
-    process.split(config, root_folder, config_file_name)
-    assert config['splitted'] is True
-    assert len(config['transformed_images']) == 2
-    check_image(root_folder, config['transformed_images'][0], 'assisted-split-{}-3'.format(type_))
-    check_image(root_folder, config['transformed_images'][1], 'assisted-split-{}-4'.format(type_))
+    step = process.split(config, step, root_folder)
+    assert len(step['sources']) == 2
+    check_image(root_folder, step['sources'][0], 'assisted-split-{}-3'.format(type_))
+    check_image(root_folder, step['sources'][1], 'assisted-split-{}-4'.format(type_))
+    assert step['name'] == 'finalise'
+    process.finalise(config, step, root_folder)
+    pdfinfo = process.output(['pdfinfo', os.path.join(root_folder, 'final.pdf')]).split('\n')
+    regex = re.compile(r'([a-zA-Z ]+): +(.*)')
+    pdfinfo = [regex.match(e) for e in pdfinfo]
+    pdfinfo = dict([e.groups() for e in pdfinfo if e is not None])
+    assert pdfinfo['Title'] == 'Test title 1'
+    assert pdfinfo['Pages'] == '2'
+    assert pdfinfo['Page size'] == size
+    process.call([
+        'gm', 'convert', os.path.join(root_folder, 'final.pdf'),
+        '+adjoin', os.path.join(root_folder, 'final.png')
+    ])
+    check_image(root_folder, step['sources'][1], 'assisted-split-{}-4'.format(type_))
 
 
 # @pytest.mark.skip(reason='for test')
@@ -115,12 +135,18 @@ def test_assisted_split_join_full():
         'args': {
             'assisted_split': True,
             'level': True,
+            'append_credit_card': False,
         },
-        'images': ['image-1.png', 'image-2.png']
+        'full_name': 'Test title 2',
+        'destination': os.path.join(root_folder, 'final.pdf'),
+    }
+    step = {
+        'sources': ['image-1.png', 'image-2.png'],
     }
     config_file_name = os.path.join(root_folder, 'config.yaml')
-    images = process.transform(config, config_file_name, root_folder)
-    config['transformed_images'] = images
+    step = process.transform(config, step, config_file_name, root_folder)
+    assert step['name'] == 'split'
+    images = step['sources']
     assert os.path.basename(images[0]) == config['assisted_split'][0]['image']
     assert len(images) == 2
     for number, elements in enumerate([({
@@ -135,10 +161,24 @@ def test_assisted_split_join_full():
         limit, destinations = elements
         config['assisted_split'][number]['limits'] = [limit]
         config['assisted_split'][number]['destinations'] = destinations
-    process.split(config, root_folder, config_file_name)
-    assert config['splitted'] is True
-    assert len(config['transformed_images']) == 1
-    check_image(root_folder, config['transformed_images'][0], 'assisted-split-join-1')
+    step = process.split(config, step, root_folder)
+    assert step['name'] == 'finalise'
+    assert len(step['sources']) == 1
+    check_image(root_folder, step['sources'][0], 'assisted-split-join-1')
+
+    process.finalise(config, step, root_folder)
+    pdfinfo = process.output(['pdfinfo', os.path.join(root_folder, 'final.pdf')]).split('\n')
+    regex = re.compile(r'([a-zA-Z ]+): +(.*)')
+    pdfinfo = [regex.match(e) for e in pdfinfo]
+    pdfinfo = dict([e.groups() for e in pdfinfo if e is not None])
+    assert pdfinfo['Title'] == 'Test title 2'
+    assert pdfinfo['Pages'] == '1'
+    assert pdfinfo['Page size'] == '612 x 229 pts'
+    process.call([
+        'gm', 'convert', os.path.join(root_folder, 'final.pdf'),
+        '+adjoin', os.path.join(root_folder, 'final.png')
+    ])
+    check_image(root_folder, step['sources'][0], 'assisted-split-join-1')
 
 
 # @pytest.mark.skip(reason='for test')
@@ -154,12 +194,17 @@ def test_full(progress, experimental):
         'args': {
             'assisted_split': False,
             'level': True,
+            'append_credit_card': False,
         },
-        'images': [os.path.join(os.path.dirname(__file__), 'all-1.png')]
+        'full_name': 'Test title 3',
+        'destination': os.path.join(root_folder, 'final.pdf'),
     }
-    images = process.transform(config, '/tmp/test-config.yaml', root_folder)
-    check_image(root_folder, images[0], 'all-1')
-    assert len(images) == 1
+    step = {
+        'sources': [os.path.join(os.path.dirname(__file__), 'all-1.png')]
+    }
+    step = process.transform(config, step, '/tmp/test-config.yaml', root_folder)
+    check_image(root_folder, step['sources'][0], 'all-1')
+    assert len(step['sources']) == 1
 
     if progress == 'TRUE':
         assert os.path.exists(os.path.join(root_folder, '0-force-cleanup/all-1.png'))
@@ -169,3 +214,18 @@ def test_full(progress, experimental):
         assert os.path.exists(os.path.join(root_folder, 'scantailor/all-1.png'))
     else:
         assert not os.path.exists(os.path.join(root_folder, 'scantailor'))
+
+    assert step['name'] == 'finalise'
+    process.finalise(config, step, root_folder)
+    pdfinfo = process.output(['pdfinfo', os.path.join(root_folder, 'final.pdf')]).split('\n')
+    regex = re.compile(r'([a-zA-Z ]+): +(.*)')
+    pdfinfo = [regex.match(e) for e in pdfinfo]
+    pdfinfo = dict([e.groups() for e in pdfinfo if e is not None])
+    assert pdfinfo['Title'] == 'Test title 3'
+    assert pdfinfo['Pages'] == '1'
+    assert pdfinfo['Page size'] == '309 x 792 pts'
+    process.call([
+        'gm', 'convert', os.path.join(root_folder, 'final.pdf'),
+        '+adjoin', os.path.join(root_folder, 'final.png')
+    ])
+    check_image(root_folder, os.path.join(root_folder, 'final.png'), 'all-2')
