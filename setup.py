5ff3cc53d30ed948ee7b36ceb385ed60d4937183
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-08-18 20:45:11 +0200

    Format

--- setup.py
+++ setup.py
@@ -3,31 +3,28 @@
 
 import os
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 HERE = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(HERE, 'README.md')) as f:
+with open(os.path.join(HERE, "README.md")) as f:
     README = f.read()
 
 setup(
-    name='scan-to-paperless',
-    version='0.11.0',
-    description='Tool to scan and process documents to palerless',
+    name="scan-to-paperless",
+    version="0.11.0",
+    description="Tool to scan and process documents to palerless",
     long_description=README,
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-    ],
-    author='Stéphane Brunner',
-    author_email='stephane.brunner@gmail.com',
-    url='https://hub.docker.com/r/sbrunner/scan-to-paperless/',
-    packages=find_packages(exclude=['tests.*']),
-    install_requires=['argcomplete', 'pyyaml', 'scikit-image'],
+    long_description_content_type="text/markdown",
+    classifiers=["Programming Language :: Python", "Programming Language :: Python :: 3",],
+    author="Stéphane Brunner",
+    author_email="stephane.brunner@gmail.com",
+    url="https://hub.docker.com/r/sbrunner/scan-to-paperless/",
+    packages=find_packages(exclude=["tests.*"]),
+    install_requires=["argcomplete", "pyyaml", "scikit-image"],
     entry_points={
-        'console_scripts': [
-            'scan = scan_to_paperless.scan:main',
-            'scan-process-status = scan_to_paperless.scan_process_status:main',
+        "console_scripts": [
+            "scan = scan_to_paperless.scan:main",
+            "scan-process-status = scan_to_paperless.scan_process_status:main",
         ],
     },
 )
