32abd52eaa26ffaab450bdcc620ea7eaf5bfe526
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-06-28 21:59:42 +0200

    Use a Jinja template

--- scan_to_paperless/status.py
+++ scan_to_paperless/status.py
@@ -6,6 +6,7 @@ import html
 import os.path
 from typing import Dict, NamedTuple, Optional
 
+from jinja2 import Environment, PackageLoader, select_autoescape
 from ruamel.yaml.main import YAML
 
 WAITING_STATUS_NAME = "Waiting validation"
@@ -169,105 +170,14 @@ class Status:
             return
 
         with open(self._file, "w", encoding="utf-8") as status_file:
+            env = Environment(loader=PackageLoader("scan_to_paperless"), autoescape=select_autoescape())
+            template = env.get_template("status.html")
             status_file.write(
-                f"""<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Scan to Paperless status</title>
-    <link
-      rel="icon"
-      type="image/svg"
-      color="lighthex"
-      href="https://raw.githubusercontent.com/sbrunner/scan-to-paperless/test/logo.svg"
-      referrerpolicy="no-referrer"
-    />
-    <link
-      rel="icon"
-      type="image/svg"
-      color="lighthex"
-      media="(prefers-color-scheme: dark)"
-      href="https://raw.githubusercontent.com/sbrunner/scan-to-paperless/test/logo-dark.svg"
-      referrerpolicy="no-referrer"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css"
-      integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css"
-      integrity="sha512-ZnR2wlLbSbr8/c9AgLg3jQPAattCUImNsae6NHYnS9KrIwRdcY9DxFotXhNAKIKbAXlRnujIqUWoXXwqyFOeIQ=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.21.4/bootstrap-table.min.css"
-      integrity="sha512-vaPSEKPBIWEWK+pGIwdLnPzw7S2Tr6rYVT05v+KN89YVpEJavFiY1dPzT+e1ZeyizjEPBicVxJ5QixXZw0Nopw=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    />
-  </head>
-  <body class="px-5 py-4">
-    <h1>Scan to Paperless status</h1>
-    <p><em>{self._global_status}</em> since <script>
-    window.document.write(new Date('{self._global_status_update.isoformat()}Z').toLocaleString());
-    </script></p>
-    <p>Started at: <script>
-    window.document.write(new Date('{self._start_time.isoformat()}Z').toLocaleString());
-    </script></p>
-    <p>Generated at: <script>
-    window.document.write(new Date('{datetime.datetime.utcnow().replace(microsecond=0).isoformat()}Z').toLocaleString());
-    </script></p>
-    <h2>Jobs</h2>
-    <table data-toggle="table">
-      <thead>
-        <tr>
-          <th data-sortable="true">Folder</th>
-          <th data-sortable="true">Status</th>
-          <th>Details</th>
-        </tr>
-      </thead>
-      <tbody>
-"""
-            )
-            for name, folder in self._status.items():
-                tr_attributes = ' class="alert alert-info"' if name == self._current_folder else ""
-                status_file.write(
-                    f"""        <tr{tr_attributes}>
-          <td><a href="./{name}" target="_blank">{name}</a></td>
-          <td>{folder.status}</td>
-          <td>{folder.details}</td>
-        </tr>
-"""
+                template.render(
+                    global_status=self._global_status,
+                    global_status_update=self._global_status_update,
+                    start_time=self._start_time,
+                    datetime=datetime,
+                    status=self._status,
                 )
-            status_file.write(
-                """      </tbody>
-    </table>
-
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
-      integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    ></script>
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"
-      integrity="sha512-VK2zcvntEufaimc+efOYi622VN5ZacdnufnmX7zIhCPmjhKnOi9ZDMtg1/ug5l183f19gG1/cBstPO4D8N/Img=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    ></script>
-    <script
-      src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.21.4/bootstrap-table.min.js"
-      integrity="sha512-rZAhvMayqW5e/N+xdp011tYAIdxgMMJtKxUXx7scO4iBPSUXAKdkrKIPRu6tLr0O9V6Bs9QujJF3MqmgSNfYPA=="
-      crossorigin="anonymous"
-      referrerpolicy="no-referrer"
-    ></script>
-  </body>
-</html>"""
             )
