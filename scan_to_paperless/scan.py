555c93e50866e43843ac7dff893cbe380af796db
Stéphane Brunner <stephane.brunner@camptocamp.com>
2025-08-14 19:28:22 +0200

    Fix image in YAML

--- scan_to_paperless/scan.py
+++ scan_to_paperless/scan.py
@@ -238,7 +238,7 @@ def main() -> None:
     images = sorted(images, key=image_match)
     if images:
         process_config = {
-            "images": images,
+            "images": [str(image) for image in images],
             "args": args_,
         }
         yaml = YAML()
